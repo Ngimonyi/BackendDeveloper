da70061cbcba726d11ae8bd70c96b286ab840877
josaphat Mosha <ngimonyijos@gmail.com>
2024-07-02 12:06:19 +0200

    changes in osnovne_mysql/parcijalni_ispit

--- osnovne_mysql/parcijalni_ispit/company_create.sql
+++ osnovne_mysql/parcijalni_ispit/company_create.sql
@@ -1,4 +1,3 @@
-
 -- Drop the database if it exists
 DROP DATABASE IF EXISTS company;
 
@@ -8,44 +7,36 @@ CREATE DATABASE IF NOT EXISTS company DEFAULT CHARACTER SET utf8 COLLATE utf8_ge
 -- Use the database
 USE company;
 
--- Create employees table
-CREATE TABLE IF NOT EXISTS employees (
+-- Create the employee table
+CREATE TABLE IF NOT EXISTS employee (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(45) NOT NULL,
-    last_name VARCHAR(45) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(20) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    employee_name VARCHAR(45) NOT NULL,
+    employee_surname VARCHAR(45) NOT NULL,
+    employee_address VARCHAR(45) NOT NULL,
+    phone VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
     salary DECIMAL(10,2) NOT NULL
 );
 
--- Create departments table
-CREATE TABLE IF NOT EXISTS departments (
+-- Create the department table
+CREATE TABLE IF NOT EXISTS department (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    department_name VARCHAR(45) NOT NULL
+    department_name VARCHAR(45) NOT NULL,
+    leader INT UNSIGNED NOT NULL,
+    FOREIGN KEY (leader) REFERENCES employee(id)
 );
 
--- Create employee_departments table (many-to-many relationship)
-CREATE TABLE IF NOT EXISTS employee_departments (
+-- Create the employee_department table (many-to-many relationship)
+CREATE TABLE IF NOT EXISTS employee_department (
     employee_id INT UNSIGNED NOT NULL,
     department_id INT UNSIGNED NOT NULL,
-    PRIMARY KEY (employee_id, department_id),
-    FOREIGN KEY (employee_id) REFERENCES employees(id),
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    FOREIGN KEY (employee_id) REFERENCES employee(id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
--- Create department_leaders table (to store leaders of departments)
-CREATE TABLE IF NOT EXISTS department_leaders (
-    department_id INT UNSIGNED NOT NULL PRIMARY KEY,
-    leader_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES departments(id),
-    FOREIGN KEY (leader_id) REFERENCES employees(id)
-);
-
-
--- Insert data into employees table
-INSERT INTO employees (first_name, last_name, address, phone_number, email, salary) VALUES
-    ('Ndaroi', 'Domo', '123 Main St', '555-1234', 'john.doe@example.com', 1000.00),
+-- Insert data into employee table
+INSERT INTO employee (employee_name, employee_surname, employee_address, phone, email, salary) VALUES
+    ('Ndaroi', 'Domo', '123 Main St', '555-1234', 'ndaroi.domo@example.com', 1000.00),
     ('Jane', 'Smith', '456 Oak Ave', '555-5678', 'jane.smith@example.com', 950.00),
     ('Michael', 'Johnson', '789 Elm Blvd', '555-9012', 'michael.johnson@example.com', 1050.00),
     ('Emily', 'Williams', '101 Pine Rd', '555-3456', 'emily.williams@example.com', 1050.00),
@@ -57,16 +48,16 @@ INSERT INTO employees (first_name, last_name, address, phone_number, email, sala
     ('Jessica', 'Rodriguez', '707 Apple St', '555-2109', 'jessica.rodriguez@example.com', 700.00),
     ('Matthew', 'Hernandez', '808 Banana Ave', '555-6543', 'matthew.hernandez@example.com', 1000.00);
 
--- Insert data into departments table
-INSERT INTO departments (department_name) VALUES
-    ('Sales'),
-    ('Marketing'),
-    ('Finance'),
-    ('Warehouse'),
-    ('IT');
+-- Insert data into department table
+INSERT INTO department (department_name, leader) VALUES
+    ('Sales', 4),
+    ('Marketing', 4),
+    ('Finance', 5),
+    ('Warehouse', 8),
+    ('IT', 9);
 
--- Insert data into employee_departments table
-INSERT INTO employee_departments (employee_id, department_id) VALUES
+-- Insert data into employee_department table
+INSERT INTO employee_department (employee_id, department_id) VALUES
     (1, 1),
     (1, 2),
     (2, 1),
@@ -82,47 +73,98 @@ INSERT INTO employee_departments (employee_id, department_id) VALUES
     (10, 5),
     (11, 5);
 
--- Insert data into department_leaders table
-INSERT INTO department_leaders (department_id, leader_id) VALUES
-    (1, 4), -- Sales led by Michael Johnson
-    (2, 4), -- Marketing led by Michael Johnson
-    (3, 5), -- Finance led by David Brown
-    (4, 8), -- Warehouse led by Maria Martinez
-    (5, 9); -- IT led by Christopher Davis
-
-
-
 -- Retrieve all employees and their salaries
-SELECT CONCAT(first_name, ' ', last_name) AS 'Full Name', salary AS 'Salary'
-FROM employees;
+SELECT CONCAT(employee_name, ' ', employee_surname) AS 'Full Name', salary AS 'Salary'
+FROM employee; 
 
--- Fetch all department leaders and calculate the average of their salaries
-SELECT d.department_name AS 'Department', ROUND(AVG(e.salary), 2) AS 'Average Salary'
-FROM departments d
-JOIN department_leaders dl ON d.id = dl.department_id
-JOIN employees e ON dl.leader_id = e.id
-GROUP BY d.department_name;
+Results:
+-- +---------------------+---------+
+-- | Full Name           | Salary  |
+-- +---------------------+---------+
+-- | Ndaroi Domo         | 1000.00 |
+-- | Jane Smith          | 950.00  |
+-- | Michael Johnson     | 1050.00 |
+-- | Emily Williams      | 1050.00 |
+-- | David Brown         | 1050.00 |
+-- | Sarah Jones         | 700.00  |
+-- | Daniel Garcia       | 1050.00 |
+-- | Maria Martinez      | 1200.00 |
+-- | Christopher Davis   | 1500.00 |
+-- | Jessica Rodriguez   | 700.00  |
+-- | Matthew Hernandez   | 1000.00 |
+-- +---------------------+---------+
+
+-- Retrieve all department leaders and calculate the average of their salaries
+SELECT ROUND(SUM(e.salary) / COUNT(e.id), 2) AS 'Average Salary of Leaders'
+FROM employee e
+JOIN department d ON d.leader = e.id;
+
+Results:
+-- +---------------------------+
+-- | Average Salary of Leaders |
+-- +---------------------------+
+-- |                   1170.00 |
+-- +---------------------------+
 
 -- Create a procedure to calculate the average salary of all employees
 DELIMITER $$
-CREATE PROCEDURE IF NOT EXISTS calculate_average_salary()
+
+CREATE PROCEDURE IF NOT EXISTS calculate_average_salary() 
 BEGIN
-    SELECT ROUND(AVG(salary), 2) AS 'Average Salary' FROM employees;
+    SELECT ROUND(AVG(salary), 2) AS 'Average Salary' FROM employee;
 END $$
+
 DELIMITER ;
 
 -- Example of calling the stored procedure to calculate average salary
 CALL calculate_average_salary();
 
--- Fetch employees by departments
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee, d.department_name AS 'Department'
-FROM employees e
-JOIN employee_departments ed ON e.id = ed.employee_id
-JOIN departments d ON ed.department_id = d.id
-ORDER BY d.department_name, e.last_name;
+Results:
+-- +-----------------+
+-- | Average Salary  |
+-- +-----------------+
+-- |         1027.27 |
+-- +-----------------+
 
--- Fetch department leaders
-SELECT d.department_name AS 'Department', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Leader'
-FROM departments d
-JOIN department_leaders dl ON d.id = dl.department_id
-JOIN employees e ON dl.leader_id = e.id;
+-- Employees by department
+SELECT CONCAT(e.employee_name, ' ', e.employee_surname) AS Employee, d.department_name AS 'Department'
+FROM employee e
+JOIN employee_department ed ON e.id = ed.employee_id
+JOIN department d ON d.id = ed.department_id
+GROUP BY Employee, d.department_name;
+
+Results:
+-- +------------------+--------------+
+-- | Employee         | Department   |
+-- +------------------+--------------+
+-- | Ndaroi Domo      | Sales        |
+-- | Ndaroi Domo      | Marketing    |
+-- | Jane Smith       | Sales        |
+-- | Jane Smith       | Sales        |
+-- | Michael Johnson  | Sales        |
+-- | Emily Williams   | Sales        |
+-- | Emily Williams   | Marketing    |
+-- | David Brown      | Finance      |
+-- | Sarah Jones      | Finance      |
+-- | Daniel Garcia    | Finance      |
+-- | Maria Martinez   | Warehouse    |
+-- | Christopher Davis| IT           |
+-- | Jessica Rodriguez| IT           |
+-- | Matthew Hernandez| IT           |
+-- +------------------+--------------+
+
+-- Department leaders
+SELECT d.department_name AS 'Department', CONCAT(e.employee_name, ' ', e.employee_surname) AS 'Leader'
+FROM department d
+JOIN employee e ON e.id = d.leader;
+
+Results:
+-- +--------------+-----------------+
+-- | Department   | Leader          |
+-- +--------------+-----------------+
+-- | Sales        | Emily Williams  |
+-- | Marketing    | Emily Williams  |
+-- | Finance      | David Brown     |
+-- | Warehouse    | Maria Martinez  |
+-- | IT           | Christopher Davis|
+-- +--------------+-----------------+
